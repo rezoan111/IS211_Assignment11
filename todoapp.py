1927f9bbd7f43b6c9a85ce72886f9a8de3411fd9
rezoan111 <rezoan111@gmail.com>
2026-04-15 22:27:44 -0400

    todoapp.py

--- todoapp.py
+++ todoapp.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Temporary fake list for testing the table
+todo_items = [
+    {'task': 'Buy Milk', 'email': 'test1@example.com', 'priority': 'High'},
+    {'task': 'Finish Homework', 'email': 'test2@example.com', 'priority': 'Medium'}
+]
+
 @app.route('/')
-def hello_world():
-    # Show a simple test page first
-    return 'Hello World!'
+def show_list():
+    # Show the current to do items in the HTML table
+    return render_template('index.html', todo_items=todo_items)
 
 if __name__ == '__main__':
     # Run the Flask app
